5970ebe7c48d512d088d86e3eed6a65a1b586126
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2017-10-29 12:53:12 +0500

    Passing "db" to all handlers. Redirect to "/" when query is empty.

--- app.py
+++ app.py
@@ -14,58 +14,60 @@ try:
 except ImportError:
     pass
 
-results_per_page = int(os.getenv("RESULTS_PER_PAGE", "10"))
-
-client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
-db = client[os.getenv("MONGODB_BASE_NAME", "grapefruit")]
+RESULTS_PER_PAGE = 10
 
 
 @aiohttp_jinja2.template("index.html")
-async def render_home(request):
+async def render_home(db, request):
     _, torrents_count = await get_torrents_count(db)
     return {"torrents_count": torrents_count}
 
 
-async def render_latest(request):
+async def render_latest(db, request):
     page = int(request.match_info.get("page", "1"))
 
     elapsed_time, (results_count, results) = await get_last_torrents(
-        db, ["name", "files", "info_hash"], (min(page, 10) - 1) * results_per_page, results_per_page
+        db, ["name", "files", "info_hash"], (min(page, 10) - 1) * RESULTS_PER_PAGE, RESULTS_PER_PAGE
     )
 
     response = await render_results("/latest", "", page, results, min(results_count, 100), elapsed_time, request)
     return response
 
 
-async def render_query(request):
+async def render_query(db, request):
     query = request.match_info.get("query", None)
     page = int(request.match_info.get("page", "1"))
 
     if query:
         elapsed_time, (results_count, results) = await search_torrents(
-            db, query, ["name", "files", "info_hash"], (page - 1) * results_per_page, results_per_page
+            db, query, ["name", "files", "info_hash"], (page - 1) * RESULTS_PER_PAGE, RESULTS_PER_PAGE
         )
 
         response = await render_results("/search", query, page, results, results_count, elapsed_time, request)
         return response
-    else:
+    elif request.query.get("q", None):
         url = "/search/{0}".format(request.query["q"])
         return web.HTTPFound(url)
+    else:
+        return web.HTTPFound("/")
 
 
 @aiohttp_jinja2.template("details.html")
-async def render_torrent(request):
-    info_hash = request.match_info["info_hash"]
+async def render_torrent(db, request):
+    info_hash = request.match_info.get("info_hash", None)
 
-    _, result = await get_torrent_details(db, info_hash)
+    if info_hash:
+        _, result = await get_torrent_details(db, info_hash)
 
-    return {
-        "query": result["name"],
-        "title": result["name"],
-        "size": get_files_size(result["files"]),
-        "info_hash": result["info_hash"],
-        "files": get_files_list(result["files"])
-    }
+        return {
+            "query": result["name"],
+            "title": result["name"],
+            "size": get_files_size(result["files"]),
+            "info_hash": result["info_hash"],
+            "files": get_files_list(result["files"])
+        }
+    else:
+        return web.HTTPFound("/")
 
 
 @aiohttp_jinja2.template("results.html")
@@ -74,7 +76,7 @@ async def render_results(source_url, query, page, results, results_count, elapse
         "source_url": source_url,
         "query": query,
         "page": page,
-        "total_pages": int(math.ceil(results_count / results_per_page)),
+        "total_pages": int(math.ceil(results_count / RESULTS_PER_PAGE)),
         "total_count": results_count,
         "time_elapsed": round(elapsed_time, 3),
         "results": map(lambda item: {
@@ -110,18 +112,25 @@ async def error_middleware(app, handler):
     return middleware_handler
 
 
-app = web.Application(middlewares=[error_middleware])
-aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
-app.router.add_static("/static", "static", name="static")
-app.router.add_get("/", render_home)
-app.router.add_get("/latest", render_latest)
-app.router.add_get("/latest/{page}", render_latest)
-app.router.add_get("/search", render_query)
-app.router.add_get("/search/{query}", render_query)
-app.router.add_get("/search/{query}/{page}", render_query)
-app.router.add_get("/torrent/{info_hash}", render_torrent)
+if __name__ == '__main__':
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(create_indexes(db))
+    client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+    db = client[os.getenv("MONGODB_BASE_NAME", "grapefruit")]
 
-web.run_app(app, host="127.0.0.1", loop=loop)
+    app = web.Application(middlewares=[error_middleware])
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
+
+    app.router.add_static("/static", "static", name="static")
+    app.router.add_get("/", lambda request: render_home(db, request))
+    app.router.add_get("/latest", lambda request: render_latest(db, request))
+    app.router.add_get("/latest/{page}", lambda request: render_latest(db, request))
+    app.router.add_get("/search", lambda request: render_query(db, request))
+    app.router.add_get("/search/{query}", lambda request: render_query(db, request))
+    app.router.add_get("/search/{query}/{page}", lambda request: render_query(db, request))
+    app.router.add_get("/torrent/{info_hash}", lambda request: render_torrent(db, request))
+
+    loop.run_until_complete(create_indexes(db))
+
+    web.run_app(app, host="0.0.0.0", loop=loop)
